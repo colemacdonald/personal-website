eab7427bd21355cb5a8d30c36c80668ed1cef562
Cole Macdonald <colesinbox@gmail.com>
2020-07-03 11:03:19 -0700

    Different image used when in the air

--- src/js/wize/js/game/KYeezyCharacter.js
+++ src/js/wize/js/game/KYeezyCharacter.js
@@ -2,16 +2,25 @@ import { ControllableCharacter, DIRECTIONS } from "./ControllableCharacter.js";
 import { _ } from "underscore";
 
 let STATES = {
+  IDLE: "IDLE",
+  WALKING: "WALKING",
+  JUMPING: "JUMPING",
+  INAIR: "INAIR",
+};
+
+let FRAMES = {
   IDLE: {
     right: {
-      sources: [],
+      sources: ["resources/wize/kyell_still_right.png"],
       images: [],
       x_offset: 0,
+      width_extend: 0,
     },
     left: {
-      sources: [],
+      sources: ["resources/wize/kyell_still_left.png"],
       images: [],
       x_offset: 0,
+      width_extend: 0,
     },
   },
   WALKING: {
@@ -99,14 +108,22 @@ let STATES = {
   },
   INAIR: {
     right: {
-      sources: [],
+      sources: [
+        // "resources/wize/kyell_jump/kyell_jump_right0000.png",
+        // "resources/wize/kyell_jump/kyell_jump_right0001.png",
+        // "resources/wize/kyell_jump/kyell_jump_right0002.png",
+        // "resources/wize/kyell_jump/kyell_jump_right0003.png",
+        "resources/wize/kyell_jump/kyell_jump_right0004.png",
+      ],
       images: [],
       x_offset: 0,
+      width_extend: 0,
     },
     left: {
-      sources: [],
+      sources: ["resources/wize/kyell_jump/kyell_jump_left0004.png"],
       images: [],
       x_offset: 0,
+      width_extend: 0,
     },
   },
 };
@@ -126,50 +143,62 @@ class KYeezy extends ControllableCharacter {
     this.hurtBoxes = [{ x: 5, y: 10, h: 70, w: 30 }];
     this.hitBoxes = [];
 
+    // For the getFrame fsm
+    this.state = STATES.IDLE;
+
     // Bootstrap Images
-    _.each(STATES, function (state) {
-      state.right.images = [];
-      _.each(state.right.sources, function (imgSrc) {
+    _.each(FRAMES, function (frameGroup) {
+      frameGroup.right.images = [];
+      _.each(frameGroup.right.sources, function (imgSrc) {
         let img = new Image();
         img.src = imgSrc;
-        state.right.images.push(img);
+        frameGroup.right.images.push(img);
       });
 
-      state.left.images = [];
-      _.each(state.left.sources, function (imgSrc) {
+      frameGroup.left.images = [];
+      _.each(frameGroup.left.sources, function (imgSrc) {
         let img = new Image();
         img.src = imgSrc;
-        state.left.images.push(img);
+        frameGroup.left.images.push(img);
       });
     });
   }
 
-  getFrame() {
-    var frameGroup;
+  move() {
+    super.move();
 
-    // Find group
-    if (this.onG && this.xv !== 0) {
-      frameGroup = STATES.WALKING;
+    this.updateState();
 
-      // Update counter
-      this.frameCounter =
-        (this.frameCounter + 1) % frameGroup[this.direction].images.length;
-      return {
-        img: frameGroup[this.direction].images[this.frameCounter],
-        x_offset: frameGroup[this.direction].x_offset,
-        width_extend: frameGroup[this.direction].width_extend,
-      };
+    this.frameCounter++;
+  }
+
+  updateState() {
+    /// landing
+    if (this.state === STATES.INAIR && this.xv === 0) {
     }
 
-    if (this.direction === DIRECTIONS.LEFT) {
-      let img = new Image();
-      img.src = "resources/wize/kyell_still_left.png";
-      return { img: img, x_offset: 0, width_extend: 0 };
+    if (this.onG) {
+      if (this.xv === 0) {
+        this.state = STATES.IDLE;
+      } else {
+        this.state = STATES.WALKING;
+      }
     } else {
-      let img = new Image();
-      img.src = "resources/wize/kyell_still_right.png";
-      return { img: img, x_offset: 0, width_extend: 0 };
+      this.state = STATES.INAIR;
     }
+  }
+
+  getFrame() {
+    let frameGroup = FRAMES[this.state];
+
+    // Make sure counter fits in our current state
+    this.frameCounter =
+      this.frameCounter % frameGroup[this.direction].images.length;
+    return {
+      img: frameGroup[this.direction].images[this.frameCounter],
+      x_offset: frameGroup[this.direction].x_offset,
+      width_extend: frameGroup[this.direction].width_extend,
+    };
   }
 }
 
