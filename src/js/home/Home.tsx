67057b8e3e181dabbdbcc1ba999062adf4514a7f
Cole Macdonald <colesinbox@gmail.com>
2022-03-17 16:54:39 -0700

    updated nav styling; fixed scroll on main page

--- src/js/home/Home.tsx
+++ src/js/home/Home.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 const Home = () => 
-    <div className="home tab-content">
-        <div>
-            <p className="subtitle">Welcome! My name is Cole Macdonald; professionally, I am a Software Engineer, personally, I enjoy music, video games, rock climbing, and practicing my Rubik's cube speed solving.</p>
+    <div className="tab-content vertical-flex-box home">
+        <div className="vertical-flex-box centered">
+            <p className="subtitle">Welcome! My name is Cole Macdonald.</p>
+            <p>Professionally, I am a Software Engineer, personally, I enjoy music, video games, rock climbing, and practicing my Rubik's cube speed solving.</p>
         </div>
-        <div className="horizontal-flex-box centered">
+        <div className="horizontal-flex-box centered nav-buttons">
             <a className="giant-button" href="#/thehuman">
                 <p>THE HUMAN</p>
             </a>
