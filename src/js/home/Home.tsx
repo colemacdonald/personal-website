ba518a225675a997823d82287d4af28769e73ffd
Cole Macdonald <colesinbox@gmail.com>
2022-03-17 12:22:45 -0700

    created new home page to separate personal and professional portions of the website

--- src/js/home/Home.tsx
+++ src/js/home/Home.tsx
@@ -1,19 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Home extends Component {
-    render() {
-        return (
-            <div className="tab-content home">
-                <div>
-                    <h2 className="title">Cole Macdonald, EIT</h2>
-                    <p>Thanks for coming you might be the first person here...</p>
-                    <p>Clearly a work in progress but thanks for checking it out.</p>
-                    <p>While your here, try your hand at a highscore in Yeezy the Wize!</p>
-                    <a href="#/wize">Play Now!</a>
-                </div>   
-            </div>
-        );
-    }
-}
+const Home = () => 
+    <div className="home">
+        <div className="horizontal-flex-box centered">
+            <a className="giant-button" href="#/thehuman">
+                <p>THE HUMAN</p>
+            </a>
+            <a className="giant-button" href="#/theengineer">
+                <p>THE ENGINEER</p>
+            </a>
+        </div>
+    </div>;
 
-export default Home;
+export { Home };
