1278e35266423af5289abeb4dccf69c2a9be3db3
Cole Macdonald <colesinbox@gmail.com>
2022-03-18 14:56:12 -0700

    combined nav items

--- src/js/MainNavBar.tsx
+++ src/js/MainNavBar.tsx
@@ -7,24 +7,29 @@ const MainNavBar = () =>
             <svg height={40} width={40}>
                 <rect x={28} y={2} height={30} width={5} rx={1}/>
                 <polygon points="5,35 5,17 20,5, 35,17 35,35"/>
+                <polygon className="door" points="16,35 16,25 24,25 24,35" />
             </svg>
         </a>
         <div className="ml-auto">
             <div className="horizontal-flex-box">
                 <Dropdown>
-                    <Dropdown.Toggle id="personal">The Human</Dropdown.Toggle>
+                    <Dropdown.Toggle id="nav-dropdown" className="hide-after nav-dropdown-toggle vertical-flex-box centered">
+                         <div/>
+                         <div/>
+                         <div/>
+                     </Dropdown.Toggle>
                     <Dropdown.Menu>
-                        <Dropdown.Item href="#/thehuman">Home</Dropdown.Item>
+                        <Dropdown.Item href="#/thehuman">The Human</Dropdown.Item>
+
                         <Dropdown.Divider />
+
                         <Dropdown.Item href="#/wize">Wize</Dropdown.Item>
                         <Dropdown.Item href="#/wizeabout">About Wize</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
 
-                <Dropdown>
-                    <Dropdown.Toggle id="professional">The Engineer</Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        <Dropdown.Item href="#/theengineer">Home</Dropdown.Item>
+                        <Dropdown.Divider />
+
+                        <Dropdown.Item href="#/theengineer">The Engineer</Dropdown.Item>
+
                     </Dropdown.Menu>
                 </Dropdown>
             </div>
