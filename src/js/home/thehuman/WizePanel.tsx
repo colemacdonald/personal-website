35ce523283b6be8bb7a63d5a4c113de19fc57084
Cole Macdonald <colesinbox@gmail.com>
2022-03-21 19:53:41 -0700

    Removed empty pages; added some content to music; styling consistency

--- src/js/home/thehuman/WizePanel.tsx
+++ src/js/home/thehuman/WizePanel.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ContentPage } from "../../components/ContentPage";
-import { Subtitle } from "../../components/Titles";
+import { Heading1, Title } from "../../components/Titles";
 
 let controls = [
     `← and → to Move`,
@@ -11,19 +11,22 @@ let controls = [
 const WizePanel = () => 
     <ContentPage classes="wize-info">
         <div className="content">
-            <Subtitle text="Yeezy the Wize" />
+            <Title text="Yeezy the Wize" />
 
             <p>This game was originally developed by my friend and I in 2018. The physics are all custom, and frames are drawn on an html canvas (yes, really). 2022 saw a large graphics revamp and the introduction of story mode.</p>
+
+            <div className="primary-button vertical-flex-box">
+                <a className="vertical-flex-box centered" href="#/wize"><p>Play Now!</p></a>
+            </div>
             <div>
-                Controls:
+                <Heading1 text="Controls" />
                 <ul>
                     {controls.map(c => <li>{c}</li>)}
                 </ul>
             </div>
-            <div className="primary-button vertical-flex-box left">
-                <a className="vertical-flex-box centered" href="#/wize"><p>Play Now!</p></a>
-            </div>
+            
             <div className="vertical-flex-box">
+                <Heading1 text="Game Modes" />
                 <p>In Survival mode, play through random levels collecting coins. After collecting all 20 coins you will advance to the next level.</p>
                 <img src={require("../../../resources/home/wize-survival.png")} />
                 <p>In Story mode, progress through the pre-made levels collecting powerups along the way.</p>
