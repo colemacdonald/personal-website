e00c29cc80e7cc47b77fe8ee163976b9a17bdf16
Cole Macdonald <colesinbox@gmail.com>
2023-01-07 11:59:55 -0800

    refactored personal home

--- src/js/home/thehuman/PersonalHome.tsx
+++ src/js/home/thehuman/PersonalHome.tsx
@@ -3,24 +3,19 @@ import { GiantButton } from "../../components/Buttons";
 import { Title } from "../../components/Titles";
 
 const PersonalHome = () =>
-            <div className="page personal-home home vertical-flex-box">
-                <div className="vertical-flex-box centered">
+            <div className="personal-home">
+                <div className="personal-home-hero">
                     <Title text="Cole Macdonald" />
                     <p className="subtitle">Human Being</p>
-                </div>
-                <div className="vertical-flex-box centered">
                     <p>Thanks for coming you might be the first person here...</p>
                 </div>
-                <div className="horizontal-flex-box centered nav-buttons">
+                <div className="personal-home-nav-buttons">
                     <GiantButton text="Music" href="#/thehuman/music"/>
                     <GiantButton text="Photography" href="#/thehuman/photography"/>
                     {/* <GiantButton text="Reading" href="#/thehuman/reading"/> */}
-                </div>
-                <div className="horizontal-flex-box centered nav-buttons">
                     {/* <GiantButton text="Travels" href="#/thehuman/travels"/> */}
                     {/* <GiantButton text="Sports" href="#/thehuman/sports"/> */}
                     <GiantButton text="Game Development" href="#/thehuman/wizethegame"/>
-                    
                 </div>
             </div>;
 
