809bb9c20b7d2e91f3ff77a27c5f69f62f525764
Cole Macdonald <colesinbox@gmail.com>
2023-01-08 09:40:45 -0800

    prettier

--- src/js/home/theengineer/DownloadResumeButton.tsx
+++ src/js/home/theengineer/DownloadResumeButton.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 export const DownloadResumeButton = () => (
   <div className="primary-button download-resume-button">
-    <a
-      href={require('../../../resources/home/cole-macdonald_resume_2022-01-28.pdf')}
-      target="_blank">
+    <a href={require('../../../resources/home/cole-macdonald_resume_2022-01-28.pdf')} target="_blank">
       <p>Download My Resume</p>
     </a>
   </div>
